ab83b0f913fa3634a2a4dabc422a510a1300fcb1
Sumedh Nimkarde <blesssumedh@gmail.com>
2017-02-12 12:06:27 +0530

    alexthechatbot

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 'use strict'
 
-const express = require('express')
-const bodyParser = require('body-parser')
-const request = require('request')
-const app = express()
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const app = express();
 
-app.set('port', (process.env.PORT || 5000))
+app.set('port', (process.env.PORT || 5000));
 
 // Process application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({extended: false}));
 
 // Process application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 // Index route
 app.get('/', function (req, res) {
@@ -19,7 +19,7 @@ app.get('/', function (req, res) {
     if (req.query['hub.verify_token'] == 'alex_the_bot') {
         res.send(req.query['hub.challenge'])
     }
-    res.send('Error, wrong token')
+    res.send('Error, wrong token');
 })
 
 // for Facebook verification
@@ -28,10 +28,10 @@ app.get('/webhook/', function (req, res) {
     //     res.send(req.query['hub.challenge'])
     // }
     // res.send('Error, wrong token')
-    res.send('previous webhooks used to be here.')
+    res.send('previous webhooks used to be here.');
 })
 
 // Spin up the server
 app.listen(app.get('port'), function() {
-    console.log('running on port', app.get('port'))
+    console.log('running on port', app.get('port'));
 })
