4a8e11824496d8d664e4106d923b9b4cc7127a18
Sumedh Nimkarde <blesssumedh@gmail.com>
2017-02-12 12:22:56 +0530

    alexthechatbot

--- index.js
+++ index.js
@@ -15,6 +15,11 @@ app.use(bodyParser.json());
 
 // Index route
 app.get('/', function (req, res) {
+	res.send('Hello World! This is Alex.');
+});
+
+// for Facebook verification
+app.get('/webhook/', function (req, res) {
     if (req.query['hub.mode'] === 'subscribe' &&
       req.query['hub.verify_token'] === 'alex_the_bot') {
     console.log("Validating webhook");
@@ -23,18 +28,9 @@ app.get('/', function (req, res) {
     console.error("Failed validation. Make sure the validation tokens match.");
     res.sendStatus(403);          
   }  
-})
-
-// for Facebook verification
-app.get('/webhook/', function (req, res) {
-    // if (req.query['hub.verify_token'] == 'alex_the_bot') {
-    //     res.send(req.query['hub.challenge'])
-    // }
-    // res.send('Error, wrong token')
-    res.send('previous webhooks used to be here.');
-})
+});
 
 // Spin up the server
 app.listen(app.get('port'), function() {
     console.log('running on port', app.get('port'));
-})
+});
