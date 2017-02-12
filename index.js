1cea3d5cb0da49ad49d056a52414ac67ddb15b90
Sumedh Nimkarde <blesssumedh@gmail.com>
2017-02-12 11:57:34 +0530

    alexthechatbot

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.get('/', function (req, res) {
 
 // for Facebook verification
 app.get('/webhook/', function (req, res) {
-    if (req.query['hub.verify_token'] === 'alex') {
+    if (req.query['hub.verify_token'] == 'alex_the_bot') {
         res.send(req.query['hub.challenge'])
     }
     res.send('Error, wrong token')
