c5ac121fc4fc2b8c3eecf65fff0f5681a7929e96
Sumedh Nimkarde <blesssumedh@gmail.com>
2017-02-14 19:05:51 +0530

    update index.js

--- index.js
+++ index.js
@@ -15,33 +15,59 @@ app.use(bodyParser.urlencoded({extended: false}));
 // Process application/json
 app.use(bodyParser.json());
 
-const VALIDATION_TOKEN = (process.env.MESSENGER_VALIDATION_TOKEN) ?
-  (process.env.MESSENGER_VALIDATION_TOKEN) :
-  'alex';
-
+const VALIDATION_TOKEN = 'alex';
 
 // Index route
 app.get('/', function (req, res) {
-  console.log(req.query);
-  // console.log(req.query['hub.verify_token']);
+  res.status(200).send('ok');
+});
+
+// for Facebook verification
+app.get('/webhook/', function(req, res) {
+    // console.log(req.query['hub.verify_token']);
   if (req.query['hub.verify_token'] === VALIDATION_TOKEN) {
     console.log("Validating webhook");
     res.status(200).send(req.query['hub.challenge']);
   } else {
     console.error("Failed validation. Make sure the validation tokens match.");
-    console.log(res.body);
     res.sendStatus(403);          
   }
 });
 
-// for Facebook verification
-// app.get('/webhook/', function(req, res) {
-    
-// });
+app.post('/webhook', function(req, res){
+	// console.log(req.body);
+  var data = req.body;
 
-app.post('/', function(req, res){
-	console.log(req.body);
+  // Make sure this is a page subscription
+  if (data.object === 'page') {
+
+    // Iterate over each entry - there may be multiple if batched
+    data.entry.forEach(function(entry) {
+      var pageID = entry.id;
+      var timeOfEvent = entry.time;
+
+      // Iterate over each messaging event
+      entry.messaging.forEach(function(event) {
+        if (event.message) {
+          receivedMessage(event);
+        } else {
+          console.log("Webhook received unknown event: ", event);
+        }
+      });
+    });
+
+    // Assume all went well.
+    //
+    // You must send back a 200, within 20 seconds, to let us know
+    // you've successfully received the callback. Otherwise, the request
+    // will time out and we will keep trying to resend.
+    res.sendStatus(200);
+  }
 });
+
+function receivedMessage(event) {
+  console.log('Message data : ', event.message);
+}
 
 // Spin up the server
 app.listen(app.get('port'), function() {
